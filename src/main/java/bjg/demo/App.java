92a650b9839dbfaba7840364ea6a105e0b097149
bjg <bjg515@gmail.com>
2021-11-30 20:27:54 -0500

    First commit

---
+++ src/main/java/bjg/demo/App.java
@@ -0,0 +1,13 @@
+package bjg.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
